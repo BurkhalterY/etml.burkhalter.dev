66c59da5182c69d62fe9698690303ef00be17527
Yannis Burkhalter <yannis@burkhalter.dev>
2023-03-20 14:58:13 +0100

    Change cover

--- src/views/CoverPage.vue
+++ src/views/CoverPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="px-12 pt-12 text-white bg-right-bottom bg-cover"
-    style="background-image: url('/img/cover.png')"
+    style="background-image: url('/img/cover.webp')"
   >
     <img
       class="float-right w-1/3 xl:w-1/6 brightness-0 invert"

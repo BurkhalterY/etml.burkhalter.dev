118c28810125538d2164e0d87b2cb685914fec95
BurkhalterY <yannis.burkhalter@gmail.com>
2023-03-25 07:28:52 +0000

    Deploying to built from @ BurkhalterY/etml.burkhalter.dev@38546e1959423e2edaa324923c9b021510788653 🚀

---
+++ assets/WipPage.6afef7a0.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c}from"./index.c7d7f54a.js";const r={},s={class:"block"};function a(n,t){return o(),c("code",s,"Work in progress... more pages soon!")}var p=e(r,[["render",a]]);export{p as default};

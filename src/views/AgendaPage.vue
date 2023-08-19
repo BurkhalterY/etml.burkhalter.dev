75ac53fad7f22af2a3bb9d990d2d42b23b1955dd
Yannis Burkhalter <yannis@burkhalter.dev>
2023-08-19 13:15:57 +0200

    Same on success

--- src/views/AgendaPage.vue
+++ src/views/AgendaPage.vue
@@ -65,128 +65,133 @@ const LINES_ON_SUNDAY = 3
       :key="i"
       class="w-full xl:w-1/2 xl:pr-16"
     >
-      <h2
-        @click="
-          day.tasks.length
-            ? [
-                (popupStore.component = DayPopup),
-                (popupStore.additionalData = { day }),
-              ]
-            : []
-        "
-        class="px-1 text-white bg-orange-700"
-        :class="{ 'cursor-pointer': day.tasks.length }"
-      >
-        <span class="text-2xl font-light">
-          {{ day.date.getDate() }}
-        </span>
-        <span class="ml-2 font-light uppercase text-md">
-          {{ days[day.date.getDay()] }}
-        </span>
-        <span
-          v-if="i == 0 || day.date.getDate() == 1"
-          class="float-right text-2xl font-light"
-        >
-          {{ months[day.date.getMonth()] }}
-        </span>
-      </h2>
-      <ul>
-        <li
-          v-for="task in [...day.tasks]
-            .sort(tasksSorter)
-            .slice(
-              0,
-              (day.date.getDay() ? LINES_PER_DAY : LINES_ON_SUNDAY) - 1
-            )"
-          :key="task.id"
-          class="relative leading-relaxed truncate border-b border-orange-700"
-          :title="task.title"
-        >
-          <div
-            class="inline-block w-4 h-4 mb-1 align-middle border border-orange-700 cursor-pointer"
-          />
-          {{}}
-          <span
-            v-if="task.matter.shortName"
-            class="inline-block w-20 font-semibold"
-          >
-            {{
-              // yes, it is hardcoded...
-              task.matter.abbr == "ecdr" && route.params.promotion == "mtu2e"
-                ? "Droit"
-                : task.matter.shortName
-            }}
-          </span>
-          {{ types[task.type].emoji }} {{ task.title }}
-        </li>
-        <li class="leading-relaxed border-b border-orange-700">
-          <template
-            v-if="
-              day.tasks.length >=
-              (day.date.getDay() ? LINES_PER_DAY : LINES_ON_SUNDAY)
-            "
-          >
-            +
-            {{
-              day.tasks.length +
-              1 -
-              (day.date.getDay() ? LINES_PER_DAY : LINES_ON_SUNDAY)
-            }}
-            {{
-              day.tasks.length +
-                1 -
-                (day.date.getDay() ? LINES_PER_DAY : LINES_ON_SUNDAY) >=
-              2
-                ? "autres éléments"
-                : "autre élément"
-            }}
-            <button
-              @click="
-                day.tasks.length
-                  ? [
-                      (popupStore.component = DayPopup),
-                      (popupStore.additionalData = { day }),
-                    ]
-                  : []
-              "
-              class="mx-0.5 px-0.5 rounded text-sm text-white bg-etml"
-            >
-              Voir
-            </button>
-          </template>
-          <button
-            v-if="authStore.isAdmin"
+      <transition appear>
+        <div class="transition-opacity">
+          <h2
             @click="
-              ;[
-                (popupStore.component = TaskForm),
-                (popupStore.additionalData = {
-                  date: day.strDate,
-                  promotion: route.params.promotion,
-                  matter: '',
-                  type: 'homework',
-                  title: '',
-                }),
-              ]
+              day.tasks.length
+                ? [
+                    (popupStore.component = DayPopup),
+                    (popupStore.additionalData = { day }),
+                  ]
+                : []
             "
-            class="mx-0.5 px-0.5 rounded text-sm text-white bg-etml"
+            class="px-1 text-white bg-orange-700"
+            :class="{ 'cursor-pointer': day.tasks.length }"
           >
-            Ajouter
-          </button>
-          &nbsp;
-        </li>
-        <li
-          v-for="i in Math.max(
-            (day.date.getDay() ? LINES_PER_DAY : LINES_ON_SUNDAY) -
-              day.tasks.length -
-              1,
-            0
-          )"
-          :key="i"
-          class="leading-relaxed border-b border-orange-700"
-        >
-          &nbsp;
-        </li>
-      </ul>
+            <span class="text-2xl font-light">
+              {{ day.date.getDate() }}
+            </span>
+            <span class="ml-2 font-light uppercase text-md">
+              {{ days[day.date.getDay()] }}
+            </span>
+            <span
+              v-if="i == 0 || day.date.getDate() == 1"
+              class="float-right text-2xl font-light"
+            >
+              {{ months[day.date.getMonth()] }}
+            </span>
+          </h2>
+          <ul>
+            <li
+              v-for="task in [...day.tasks]
+                .sort(tasksSorter)
+                .slice(
+                  0,
+                  (day.date.getDay() ? LINES_PER_DAY : LINES_ON_SUNDAY) - 1
+                )"
+              :key="task.id"
+              class="relative leading-relaxed truncate border-b border-orange-700"
+              :title="task.title"
+            >
+              <div
+                class="inline-block w-4 h-4 mb-1 align-middle border border-orange-700 cursor-pointer"
+              />
+              {{}}
+              <span
+                v-if="task.matter.shortName"
+                class="inline-block w-20 font-semibold"
+              >
+                {{
+                  // yes, it is hardcoded...
+                  task.matter.abbr == "ecdr" &&
+                  route.params.promotion == "mtu2e"
+                    ? "Droit"
+                    : task.matter.shortName
+                }}
+              </span>
+              {{ types[task.type].emoji }} {{ task.title }}
+            </li>
+            <li class="leading-relaxed border-b border-orange-700">
+              <template
+                v-if="
+                  day.tasks.length >=
+                  (day.date.getDay() ? LINES_PER_DAY : LINES_ON_SUNDAY)
+                "
+              >
+                +
+                {{
+                  day.tasks.length +
+                  1 -
+                  (day.date.getDay() ? LINES_PER_DAY : LINES_ON_SUNDAY)
+                }}
+                {{
+                  day.tasks.length +
+                    1 -
+                    (day.date.getDay() ? LINES_PER_DAY : LINES_ON_SUNDAY) >=
+                  2
+                    ? "autres éléments"
+                    : "autre élément"
+                }}
+                <button
+                  @click="
+                    day.tasks.length
+                      ? [
+                          (popupStore.component = DayPopup),
+                          (popupStore.additionalData = { day }),
+                        ]
+                      : []
+                  "
+                  class="mx-0.5 px-0.5 rounded text-sm text-white bg-etml"
+                >
+                  Voir
+                </button>
+              </template>
+              <button
+                v-if="authStore.isAdmin"
+                @click="
+                  ;[
+                    (popupStore.component = TaskForm),
+                    (popupStore.additionalData = {
+                      date: day.strDate,
+                      promotion: route.params.promotion,
+                      matter: '',
+                      type: 'homework',
+                      title: '',
+                    }),
+                  ]
+                "
+                class="mx-0.5 px-0.5 rounded text-sm text-white bg-etml"
+              >
+                Ajouter
+              </button>
+              &nbsp;
+            </li>
+            <li
+              v-for="i in Math.max(
+                (day.date.getDay() ? LINES_PER_DAY : LINES_ON_SUNDAY) -
+                  day.tasks.length -
+                  1,
+                0
+              )"
+              :key="i"
+              class="leading-relaxed border-b border-orange-700"
+            >
+              &nbsp;
+            </li>
+          </ul>
+        </div>
+      </transition>
     </div>
   </div>
 </template>

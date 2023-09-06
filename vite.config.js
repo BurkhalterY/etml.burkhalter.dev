a8dc42618816bd0ad2484758531efeed54eecea8
Yannis Burkhalter <yannis@burkhalter.dev>
2023-09-06 19:15:14 +0200

    Fix manifest img

--- vite.config.js
+++ vite.config.js
@@ -18,32 +18,32 @@ export default defineConfig({
         theme_color: "#004595",
         icons: [
           {
-            src: "images/icons/etml16.png",
+            src: "img/icons/etml16.png",
             sizes: "16x16",
             type: "image/png",
           },
           {
-            src: "images/icons/etml32.png",
+            src: "img/icons/etml32.png",
             sizes: "32x32",
             type: "image/png",
           },
           {
-            src: "images/icons/etml64.png",
+            src: "img/icons/etml64.png",
             sizes: "64x64",
             type: "image/png",
           },
           {
-            src: "images/icons/etml128.png",
+            src: "img/icons/etml128.png",
             sizes: "128x128",
             type: "image/png",
           },
           {
-            src: "images/icons/etml256.png",
+            src: "img/icons/etml256.png",
             sizes: "256x256",
             type: "image/png",
           },
           {
-            src: "images/icons/etml512.png",
+            src: "img/icons/etml512.png",
             sizes: "512x512",
             type: "image/png",
           },

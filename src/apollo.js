6e5d2cfc99058b398f00f1c1a3893e312524bcad
Yannis Burkhalter <yannis@burkhalter.dev>
2023-09-13 20:56:23 +0200

    A function because await

--- src/apollo.js
+++ src/apollo.js
@@ -7,33 +7,37 @@ import { CachePersistor } from "apollo3-cache-persist"
 
 const SCHEMA_VERSION = "1"
 
-const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_API_URL,
-  fetch: (uri, options) => {
-    const token = localStorage.getItem("token")
-    if (token) options.headers["Authorization"] = token
-    return fetch(uri, options)
-  },
-})
+const getApolloClient = async () => {
+  const httpLink = createHttpLink({
+    uri: import.meta.env.VITE_API_URL,
+    fetch: (uri, options) => {
+      const token = localStorage.getItem("token")
+      if (token) options.headers["Authorization"] = token
+      return fetch(uri, options)
+    },
+  })
 
-const cache = new InMemoryCache()
+  const cache = new InMemoryCache()
 
-const persistor = new CachePersistor({
-  cache,
-  storage: window.localStorage,
-})
+  const persistor = new CachePersistor({
+    cache,
+    storage: window.localStorage,
+  })
 
-const currentVersion = window.localStorage.getItem("apollo-schema-version")
-if (currentVersion === SCHEMA_VERSION) {
-  await persistor.restore()
-} else {
-  await persistor.purge()
-  window.localStorage.setItem("apollo-schema-version", SCHEMA_VERSION)
+  const currentVersion = window.localStorage.getItem("apollo-schema-version")
+  if (currentVersion === SCHEMA_VERSION) {
+    await persistor.restore()
+  } else {
+    await persistor.purge()
+    window.localStorage.setItem("apollo-schema-version", SCHEMA_VERSION)
+  }
+
+  const client = new ApolloClient({
+    link: httpLink,
+    cache,
+  })
+
+  return client
 }
 
-const client = new ApolloClient({
-  link: httpLink,
-  cache,
-})
-
-export default client
+export default getApolloClient()

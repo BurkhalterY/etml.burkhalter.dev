66d9ff345953b82de4a0fe5e949aa4480846a574
Yannis Burkhalter <yannis@burkhalter.dev>
2023-11-03 15:03:38 +0100

    Update task sorting when same matter

--- src/utils.js
+++ src/utils.js
@@ -81,5 +81,8 @@ export const tasksSorter = (a, b) => {
   if (matterA < matterB) return -1
   if (matterA > matterB) return 1
 
+  if (a.title < b.title) return -1
+  if (a.title > b.title) return 1
+
   return 0
 }
